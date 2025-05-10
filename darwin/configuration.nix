03c5b45e1affc1a52a8d1a5971e8ca579def3b0f
Lars Halvor <larshalvorhansen1@gmail.com>
2025-05-10 04:27:09 +0200

    small fix

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,55 +1,58 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    git
-    tmux
-    python3
-    cmatrix
-    typst
-    eza
-    neofetch
-    ffmpeg
-    glow
-  ];
+	environment.systemPackages = with pkgs; [
+		git
+		tmux
+		python3
+		cmatrix
+		typst
+		eza
+		neofetch
+		ffmpeg
+		glow
+	];
 
-  services.nix-daemon.enable = true;
-  nix.package = pkgs.nix;
+	services.nix-daemon.enable = true;
+	nix.package = pkgs.nix;
 
-  programs.zsh.enable = true;
+	programs.zsh.enable = true;
 
-  homebrew.enable = true;
-  homebrew.casks = [
-    "ableton-live-standard"
-    "aerospace"
-    "alacritty"
-    "bitwarden"
-    "chatgpt"
-    "element"
-    "firefox"
-    "google-chrome"
-    "messenger"
-    "minecraft"
-    "pixelmator-pro"
-    "raspberry-pi-imager"
-    "rectangle"
-    "signal"
-    "skim"
-    "spotify"
-    "vcv-rack"
-    "balenaetcher"
-  ];
+	homebrew.enable = true;
+	homebrew.casks = [
+		"ableton-live-standard"
+		"aerospace"
+		"alacritty"
+		"bitwarden"
+		"chatgpt"
+		"element"
+		"firefox"
+		"google-chrome"
+		"messenger"
+		"minecraft"
+		"pixelmator-pro"
+		"raspberry-pi-imager"
+		"rectangle"
+		"signal"
+		"skim"
+		"spotify"
+		"vcv-rack"
+		"balenaetcher"
+	];
 
-  system.defaults = {
-    dock.autohide = true;
-    dock.orientation = "left";
-    dock.magnification = false;
-    finder.AppleShowAllFiles = true;
-    NSGlobalDomain.AppleKeyboardUIMode = 3;
-    NSGlobalDomain."com.apple.keyboard.fnState" = true;
-    NSGlobalDomain."com.apple.controlcenter" = {
-      "NSStatusItem Visible Brightness" = false;
-    };
-    notification."com.apple.ncprefs".doNotDisturb = true;
-  };
+	system.defaults = {
+		dock.autohide = true;
+		dock.orientation = "left";
+		dock.magnification = false;
+		finder.AppleShowAllFiles = true;
+		NSGlobalDomain.AppleKeyboardUIMode = 3;
+		NSGlobalDomain."com.apple.keyboard.fnState" = true;
+
+		# Remove this:
+		# NSGlobalDomain."com.apple.controlcenter" = {
+		#   "NSStatusItem Visible Brightness" = false;
+		# };
+
+		# notification."com.apple.ncprefs".doNotDisturb = true;
+	};
 }
