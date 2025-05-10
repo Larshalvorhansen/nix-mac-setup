b025bce4e083073588b0b7386493d37cc9a1a243
Lars Halvor <larshalvorhansen1@gmail.com>
2025-05-10 04:48:12 +0200

    small fix 4

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
 					{
 						home-manager.useGlobalPkgs = true;
 						home-manager.useUserPackages = true;
-						home-manager.users.lhh = import ./home/lhh.nix { inherit pkgs config; };
+						home-manager.users.lhh = import ./home/lhh.nix { inherit pkgs; };
 					}
 				];
 			};
