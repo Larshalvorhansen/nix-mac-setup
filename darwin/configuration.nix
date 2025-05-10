d511baf844c28fff13f07d973da885eb55246761
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-05-11 00:56:29 +0200

    hei

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,76 +1,79 @@
-{ config, pkgs, ... }: {
-# ... other nix-darwin system settings ...
+{ config, pkgs, ... }:
 
-# Define the user (lhh) and home for nix-darwin's sake:
-	users.users.lhh = {
-		home = "/Users/lhh";
-		description = "Main user account";
-# (You can include other user settings here if needed, 
-#  but nix-darwin will not create this user; it assumes it exists.)
-	};
-	environment.systemPackages = with pkgs; [
-		git
-			tmux
-			python3
-			cmatrix
-			typst
-			eza
-			neofetch
-			ffmpeg
-			glow
-			neovim
-	];
-#	services.nix-daemon.enable = true;
-	nix.package = pkgs.nix;
+{
+  users.users.lhh = {
+    home = "/Users/lhh";
+    description = "Main user account";
+  };
 
-	programs.zsh.enable = true;
+  environment.systemPackages = with pkgs; [
+    git
+    tmux
+    python3
+    cmatrix
+    typst
+    eza
+    neofetch
+    ffmpeg
+    glow
+    neovim
+  ];
 
-	homebrew.enable = true;
-	homebrew.casks = [
-		"ableton-live-standard"
-			"aerospace"
-			"alacritty"
-			"bitwarden"
-			"chatgpt"
-			"element"
-			"firefox"
-			"google-chrome"
-			"messenger"
-			"minecraft"
-			"pixelmator-pro"
-			"raspberry-pi-imager"
-			"signal"
-			"skim"
-			"spotify"
-			"vcv-rack"
-			"balenaetcher"
-	];
-	system.defaults = {
-		dock = {
-			autohide = true;
-			orientation = "right";
-			magnification = false;
-			tilesize = 18;
-			show-recents = false;
-			autohide-delay = 0.0;
-			autohide-time-modifier = 0.0;
-			persistent-apps = [];
-		};
+  nix.package = pkgs.nix;
+  programs.zsh.enable = true;
 
-		finder.AppleShowAllFiles = true;
+  homebrew.enable = true;
+  homebrew.casks = [
+    "ableton-live-standard"
+#    "aerospace"
+    "alacritty"
+    "bitwarden"
+    "chatgpt"
+    "element"
+    "firefox"
+    "google-chrome"
+    "messenger"
+    "minecraft"
+ #   "pixelmator-pro"
+    "raspberry-pi-imager"
+    "signal"
+    "skim"
+    "spotify"
+    "vcv-rack"
+#    "balenaetcher"
+  ];
 
-		NSGlobalDomain = {
-			AppleKeyboardUIMode = 3;
-			"com.apple.keyboard.fnState" = true;
-			_HIHideMenuBar = true;
+  system.defaults = {
+    dock = {
+      autohide = true;
+      orientation = "right";
+      magnification = false;
+      tilesize = 18;
+      show-recents = false;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.0;
+      persistent-apps = [];
+    };
 
-# From screenshot:
-			reduceMotion = true;
-			increaseContrast = true;
-# reduceTransparency = false; # default, so can be omitted
-			autoPlayGIFs = true;
-		};
-	};
+    finder.AppleShowAllFiles = true;
 
-	system.stateVersion = 6;
-		       }
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleKeyboardUIMode = 3;
+      AppleShowAllExtensions = true;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+      _HIHideMenuBar = true;
+      "com.apple.keyboard.fnState" = true;
+      #reduceMotion = true;
+    };
+  };
+
+  system.stateVersion = 6;
+}
--- flake.nix
+++ flake.nix
@@ -1,22 +1,37 @@
 {
-	description = "My macOS config with nix-darwin and Home Manager";
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-		darwin.url = "github:lnl7/nix-darwin";
-		home-manager.url = "github:nix-community/home-manager";
-		# (Follow inputs as needed, ensuring home-manager follows nixpkgs, etc.)
-	};
-	outputs = { self, nixpkgs, darwin, home-manager, ... }: {
-		darwinConfigurations."lhh-macbook" = darwin.lib.darwinSystem {
-			system = "aarch64-darwin";      
-			modules = [
-				./darwin/configuration.nix            # system config (see below)
-				home-manager.darwinModules.home-manager {
-					home-manager.useGlobalPkgs = true;
-					home-manager.useUserPackages = true;
-					home-manager.users.lhh = import ./home/lhh.nix;
-				}
-			];
-		};
-	};
+  description = "My macOS config with nix-darwin and Home Manager";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, darwin, home-manager, ... }: {
+    darwinConfigurations."lhh-macbook" = darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+
+      modules = [
+        ./darwin/configuration.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.lhh = import ./home/lhh.nix {
+            config = {};
+            pkgs = import nixpkgs { system = "aarch64-darwin"; };
+          };
+        }
+      ];
+    };
+  };
 }
