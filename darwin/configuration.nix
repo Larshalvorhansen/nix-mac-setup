96eef75839dbb2dc339cbe0d90f6c6fa4efb44a7
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-05-13 17:06:35 +0200

    sort alphabetically and install nodejs

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -2,93 +2,92 @@
 
 {
 
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	users.users.lhh = {
-		home = "/Users/lhh";
-		description = "Main user account";
-	};
+  users.users.lhh = {
+    home = "/Users/lhh";
+    description = "Main user account";
+  };
 
-	environment.systemPackages = with pkgs; [
-		git
-		zinit
-		tmux
-		tree-sitter
-		python3
-		cmatrix
-		typst
-		eza
-		neofetch
-		ffmpeg
-		glow
-		neovim
-		alacritty
-		element-desktop
-		firefox
-		google-chrome
-		#signal-desktop
-		skim
-		spotify
-		#vcv-rack
-		#bitwarden-cli  #broken
-	];
+  environment.systemPackages = with pkgs; [
+    aerospace
+    alacritty
+    cmatrix
+    element-desktop
+    eza
+    firefox
+    ffmpeg
+    git
+    glow
+    google-chrome
+    neofetch
+    neovim
+    nodejs
+    python3
+    skim
+    spotify
+    tree-sitter
+    tmux
+    typst
+    zinit
+    #bitwarden-cli  #broken
+    #signal-desktop
+    #vcv-rack
+  ];
 
-	nix.package = pkgs.nix;
-	programs.zsh.enable = true;
+  nix.package = pkgs.nix;
+  programs.zsh.enable = true;
 
-	# other apps...
-	# Optional: disable Homebrew completely
-	# homebrew.enable = false;
+  # other apps...
+  # Optional: disable Homebrew completely
+  # homebrew.enable = false;
 
-	#Apps that arent supported yet on apple-darwin
-	homebrew.enable = true;
-	homebrew.casks = [
-		"ableton-live-standard"
-		"chatgpt"
-		"messenger"
-		"minecraft"
-		"signal"
-		"raspberry-pi-imager"
-		"vcv-rack"
-	];
+  #Apps that arent supported yet on apple-darwin
+  homebrew.enable = true;
+  homebrew.casks = [
+    "ableton-live-standard"
+    "chatgpt"
+    "messenger"
+    "minecraft"
+    "raspberry-pi-imager"
+    "signal"
+    "vcv-rack"
+  ];
 
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
 
-	fonts.packages = with pkgs; [
-		nerd-fonts.jetbrains-mono
-	];
+  system.defaults = {
+    dock = {
+      autohide = true;
+      orientation = "right";
+      magnification = false;
+      tilesize = 18;
+      show-recents = false;
+      autohide-delay = 0.0;
+      autohide-time-modifier = 0.0;
+      persistent-apps = [];
+    };
 
+    finder.AppleShowAllFiles = true;
 
-	system.defaults = {
-		dock = {
-			autohide = true;
-			orientation = "right";
-			magnification = false;
-			tilesize = 18;
-			show-recents = false;
-			autohide-delay = 0.0;
-			autohide-time-modifier = 0.0;
-			persistent-apps = [];
-		};
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleKeyboardUIMode = 3;
+      AppleShowAllExtensions = true;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSDocumentSaveNewDocumentsToCloud = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+      _HIHideMenuBar = true;
+      "com.apple.keyboard.fnState" = true;
+    };
+  };
 
-		finder.AppleShowAllFiles = true;
-
-		NSGlobalDomain = {
-			AppleInterfaceStyle = "Dark";
-			AppleKeyboardUIMode = 3;
-			AppleShowAllExtensions = true;
-			InitialKeyRepeat = 15;
-			KeyRepeat = 2;
-			NSAutomaticWindowAnimationsEnabled = false;
-			NSDocumentSaveNewDocumentsToCloud = false;
-			NSNavPanelExpandedStateForSaveMode = true;
-			NSNavPanelExpandedStateForSaveMode2 = true;
-			PMPrintingExpandedStateForPrint = true;
-			PMPrintingExpandedStateForPrint2 = true;
-			_HIHideMenuBar = true;
-			"com.apple.keyboard.fnState" = true;
-		};
-	};
-
-
-	system.stateVersion = 6;
+  system.stateVersion = 6;
 }
