35c607652f5a0645e26c223fe9c04cdbd31fc116
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-05-13 16:18:25 +0200

    add flake.nix

--- flake.nix
+++ flake.nix
@@ -3,33 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    darwin = {
-      url = "github:lnl7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    darwin.url = "github:lnl7/nix-darwin";
+    home-manager.url = "github:nix-community/home-manager";
   };
 
   outputs = { self, nixpkgs, darwin, home-manager, ... }: {
-    darwinConfigurations."lhh-macbook" = darwin.lib.darwinSystem {
+    darwinConfigurations."Lars-sin-MacBook-Pro" = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
-
       modules = [
         ./darwin/configuration.nix
         home-manager.darwinModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
-          home-manager.users.lhh = import ./home/lhh.nix {
-            config = {};
-            pkgs = import nixpkgs { system = "aarch64-darwin"; };
-          };
+          home-manager.users.lhh = import ./home/lhh.nix;
         }
       ];
     };
