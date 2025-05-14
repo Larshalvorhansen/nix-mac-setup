1efe470b1b3a125eeb4d7896849479afa41d9a64
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-05-14 16:48:10 +0200

    fix nvim config issue in lhh.nix

--- home/lhh.nix
+++ home/lhh.nix
@@ -24,11 +24,11 @@
 
     shfmt
     nixfmt
-nodePackages.prettier
+    nodePackages.prettier
     go
     rustfmt
     clang-tools_16
-	];
+  ];
 
   programs.zsh = {
     enable = true;
@@ -48,12 +48,14 @@ nodePackages.prettier
 
     initExtra = ''
       # Load zinit from GitHub
-      source ${pkgs.fetchFromGitHub {
-        owner = "zdharma-continuum";
-        repo = "zinit";
-        rev = "v3.8.0";
-        sha256 = "sha256-yGUcwrwLXpsB3nzGITbi/0ycZej09GvozDoscEf7qp4=";
-      }}/zinit.zsh
+      source ${
+        pkgs.fetchFromGitHub {
+          owner = "zdharma-continuum";
+          repo = "zinit";
+          rev = "v3.8.0";
+          sha256 = "sha256-yGUcwrwLXpsB3nzGITbi/0ycZej09GvozDoscEf7qp4=";
+        }
+      }/zinit.zsh
 
       # Plugins
       zinit light zsh-users/zsh-autosuggestions
@@ -70,16 +72,20 @@ nodePackages.prettier
   programs.git.enable = true;
   programs.neovim.enable = true;
 
-  home.file.".config/nvim".source = ../.config/nvim;
-
-  xdg.configFile."alacritty/alacritty.toml".source = ../.config/alacritty/alacritty.toml;
+  home.file.".config/nvim".source =
+    config.lib.file.mkOutOfStoreSymlink /Users/lhh/nix-mac-setup/.config/nvim;
+  xdg.configFile."alacritty/alacritty.toml".source =
+    ../.config/alacritty/alacritty.toml;
   xdg.configFile."gh/config.yml".source = ../.config/gh/config.yml;
   xdg.configFile."gh/hosts.yml".source = ../.config/gh/hosts.yml;
-  xdg.configFile."configstore/update-notifier-netlify-cli.json".source = ../.config/configstore/update-notifier-netlify-cli.json;
-  xdg.configFile."neofetch/config.conf".source = ../.config/neofetch/config.conf;
+  xdg.configFile."configstore/update-notifier-netlify-cli.json".source =
+    ../.config/configstore/update-notifier-netlify-cli.json;
+  xdg.configFile."neofetch/config.conf".source =
+    ../.config/neofetch/config.conf;
   xdg.configFile."tmux/tmux.conf".source = ../.config/tmux/tmux.conf;
   xdg.configFile."tmux/cheatsheet.txt".source = ../.config/tmux/cheatsheet.txt;
-  xdg.configFile."aerospace/aerospace.toml".source = ../.config/aerospace/aerospace.toml;
+  xdg.configFile."aerospace/aerospace.toml".source =
+    ../.config/aerospace/aerospace.toml;
   xdg.configFile."yabai/yabairc".source = ../.config/yabai/yabairc;
   xdg.configFile."skhd/skhdrc".source = ../.config/skhd/skhdrc;
 
