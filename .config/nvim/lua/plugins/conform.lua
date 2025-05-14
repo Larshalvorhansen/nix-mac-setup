604b51c4b3087297a981364e51a25e9db3cb937d
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-05-14 16:43:43 +0200

    edit conform config

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,29 +1,26 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    -- Autoformat on save
-    format_on_save = {
-      lsp_fallback = true,
-      timeout_ms = 500,
-    },
-
-    -- Per-language formatter setup
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "black" },
-      javascript = { "prettier" },
-      typescript = { "prettier" },
-      json = { "prettier" },
-      html = { "prettier" },
-      css = { "prettier" },
-      yaml = { "prettier" },
-      markdown = { "prettier" },
-      sh = { "shfmt" },
-      go = { "gofmt" },
-      rust = { "rustfmt" },
-      nix = { "nixfmt" },
-      c = { "clang_format" },
-      cpp = { "clang_format" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		format_on_save = {
+			lsp_fallback = true,
+			timeout_ms = 500,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "black" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			json = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			sh = { "shfmt" },
+			go = { "gofmt" },
+			rust = { "rustfmt" },
+			nix = { "nixfmt" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+		},
+	},
 }
