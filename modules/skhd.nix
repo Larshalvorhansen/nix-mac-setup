f1faf8f2e9197a055dce25c6b59bcc672f7353a3
larshalvorhansen <larshalvorhansen1@gmail.com>
2025-08-10 13:46:20 +0200

    EDIT yabaiskhd config

--- modules/skhd.nix
+++ modules/skhd.nix
@@ -7,14 +7,14 @@
 
       skhdConfig = ''
         # Focus space 1–8 with cmd + alt + [1–8]
-        cmd + alt - 1 : yabai -m space --focus 1
-        cmd + alt - 2 : yabai -m space --focus 2
-        cmd + alt - 3 : yabai -m space --focus 3
-        cmd + alt - 4 : yabai -m space --focus 4
-        cmd + alt - 5 : yabai -m space --focus 5
-        cmd + alt - 6 : yabai -m space --focus 6
-        cmd + alt - 7 : yabai -m space --focus 7
-        cmd + alt - 8 : yabai -m space --focus 8
+        alt - 1 : yabai -m space --focus 1
+        alt - 2 : yabai -m space --focus 2
+        alt - 3 : yabai -m space --focus 3
+        alt - 4 : yabai -m space --focus 4
+        alt - 5 : yabai -m space --focus 5
+        alt - 6 : yabai -m space --focus 6
+        alt - 7 : yabai -m space --focus 7
+        alt - 8 : yabai -m space --focus 8
       '';
     };
   };
