d3ffb8c7ba753d8765976232cdf9f4f7f81ddc24
larshalvorhansen <larshalvorhansen1@gmail.com>
2026-06-20 19:17:55 +0200

    fix: force correct nvim config handling

--- home/lhh.nix
+++ home/lhh.nix
@@ -5,51 +5,11 @@
   home.homeDirectory = "/Users/lhh";
 
   home.packages = with pkgs; [
-    alacritty
-    black
-    cmatrix
-    cowsay
-    csvlens
-    discord
-    eza
-    fd
-    ffmpeg
-    fzf
-    gh
-    glow
-    go
-    gopls
-    harper
-    iamb
-    jless
-    karabiner-elements
-    kjv
-    librsvg
-    lynx
-    mas
-    netlify-cli
-    nixfmt
-    prettier
-    python3
-    ripgrep
-    rust-analyzer
-    rustfmt
-    shfmt
-    skimpdf
-    stylua
-    tldr
-    tinymist
-    tmux
-    tree
-    typescript-language-server
-    typst
-    typstyle
-    visidata
-    websocat
-    w3m
-    wget
-    yt-dlp
-    zotero
+    alacritty black cmatrix cowsay csvlens discord eza fd ffmpeg fzf gh glow
+    go gopls harper iamb jless karabiner-elements kjv librsvg lynx mas
+    netlify-cli nixfmt prettier python3 ripgrep rust-analyzer rustfmt shfmt
+    skimpdf stylua tldr tinymist tmux tree typescript-language-server typst
+    typstyle visidata websocat w3m wget yt-dlp zotero
   ];
 
   programs.zsh = {
@@ -57,30 +17,19 @@
     enableCompletion = true;
 
     shellAliases = {
-      g = "git";
-      k = "kubectl";
-      ls = "eza --icons";
-      tree = "eza --tree --icons";
-      ll = "ls -alh";
-      gs = "git status";
-      conf = "nvim $HOME/nix-mac-setup/.config";
-      nixing = "nvim $HOME/nix-mac-setup/home/lhh.nix";
-      n = "nvim";
-      t = "tmux";
+      g = "git"; k = "kubectl"; ls = "eza --icons"; tree = "eza --tree --icons";
+      ll = "ls -alh"; gs = "git status"; conf = "nvim $HOME/nix-mac-setup/.config";
+      nixing = "nvim $HOME/nix-mac-setup/home/lhh.nix"; n = "nvim"; t = "tmux";
       ta = ''tmux attach -t "$(tmux ls -F '#{session_name}' | head -n 0)"'';
-      ts = "tmux choose-tree -s";
-      todo = "cd $HOME/Documents/dailyTodo && nvim todo.md";
-      won = "networksetup -setairportpower en0 on";
-      woff = "networksetup -setairportpower en0 off";
+      ts = "tmux choose-tree -s"; todo = "cd $HOME/Documents/dailyTodo && nvim todo.md";
+      won = "networksetup -setairportpower en0 on"; woff = "networksetup -setairportpower en0 off";
       batt = "pmset -g batt";
     };
 
     initContent = ''
       source ${
         pkgs.fetchFromGitHub {
-          owner = "zdharma-continuum";
-          repo = "zinit";
-          rev = "v3.8.0";
+          owner = "zdharma-continuum"; repo = "zinit"; rev = "v3.8.0";
           sha256 = "sha256-yGUcwrwLXpsB3nzGITbi/0ycZej09GvozDoscEf7qp4=";
         }
       }/zinit.zsh
@@ -99,23 +48,19 @@
     enable = true;
     withRuby = false;
     withPython3 = false;
-    plugins = with pkgs.vimPlugins; [
-      harpoon2
-    ];
+    plugins = with pkgs.vimPlugins; [ harpoon2 ];
   };
 
-  # Neovim config (fixed)
+  # Correct way (this fixes the "outside $HOME" error)
   xdg.configFile."nvim".source =
     config.lib.file.mkOutOfStoreSymlink
       "${config.home.homeDirectory}/nix-mac-setup/.config/nvim";
 
   xdg.configFile."configstore/update-notifier-netlify-cli.json".source =
     ../.config/configstore/update-notifier-netlify-cli.json;
-
   xdg.configFile."gh/config.yml".source = ../.config/gh/config.yml;
   xdg.configFile."gh/hosts.yml".source = ../.config/gh/hosts.yml;
-  xdg.configFile."neofetch/config.conf".source =
-    ../.config/neofetch/config.conf;
+  xdg.configFile."neofetch/config.conf".source = ../.config/neofetch/config.conf;
 
   home.stateVersion = "26.05";
 }
